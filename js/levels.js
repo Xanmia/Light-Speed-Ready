33c2afdf4a28e0601b9cac5cab1f666c055900b5
Xanmia <Bkraem@gmail.com>
2014-02-06 20:24:21 -0500

    restructured level loading

---
+++ js/levels.js
@@ -0,0 +1,14 @@
+var levels = [
+{
+	level:0,
+	types:[{type:0, total:25}]
+},
+{
+	level:2,
+	types:[{type:0, total:25}, {type:1, total:25}]
+},
+{
+	level:5,
+	types:[{type:0, total:25}, {type:1, total:50}]
+}
+];
