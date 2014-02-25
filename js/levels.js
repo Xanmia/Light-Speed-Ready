d9d94910d639af3cdba39d428ff8cad95408a338
Xanmia <Bkraem@gmail.com>
2014-02-24 19:30:54 -0500

    enemy clean up

--- js/levels.js
+++ js/levels.js
@@ -1,14 +1,14 @@
 var levels = [
 {
 	level:0,
-	types:[{type:4, total:50},{type:5, total:10},{type:6, total:50}]
+	types:[{type:0, total:50},{type:1, total:10},{type:2, total:50}]
+},
+{ ///examples - these will fail at level 2000
+	level:2000,
+	types:[{type:2, total:50},  {type:0, total:50}] 
 },
 {
-	level:200,
-	types:[{type:2, total:50},  {type:0, total:50}]
-},
-{
-	level:300,
+	level:3000,
 	types:[{type:3, total:50},  {type:2, total:50}]
 }
 ];
